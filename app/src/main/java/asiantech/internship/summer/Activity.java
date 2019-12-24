391d5350a7e1174e1f3e60b67de27e2155275e69
Tai Nguyen D <tai.nguyen@asiantech.vn>
2019-12-24 11:43:23 +0700

    init project

---
+++ app/src/main/java/asiantech/internship/summer/Activity.java
@@ -0,0 +1,12 @@
+package asiantech.internship.summer;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity);
+    }
+}
