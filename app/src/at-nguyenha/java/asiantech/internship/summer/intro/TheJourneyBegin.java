f2934ab5c5b4ba17a5a15e9a7310ac3766f5b330
haxuannguyen1997201 <haxuannguyen1997201@gmail.com>
2019-12-24 17:16:44 +0700

    The Journey Beginall Exam

---
+++ app/src/at-nguyenha/java/asiantech/internship/summer/intro/TheJourneyBegin.java
@@ -0,0 +1,13 @@
+package asiantech.internship.summer.intro;
+
+public class TheJourneyBegin {
+
+    int add(int param1, int param2) {
+        /**
+         * Write a function that returns the sum of two numbers.
+         */
+        return param1+param2;
+    }
+
+
+}
