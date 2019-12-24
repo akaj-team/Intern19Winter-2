0521cd7b4da93b7554115d79d5c1d2390099b127
at-cuongle <cuong.le@asiantech.vn>
2019-12-24 17:01:49 +0700

    The Journey Begins Ex 2

--- app/src/at-cuongle/java/asiantech/internship/summer/intro/TheJourneyBegins.java
+++ app/src/at-cuongle/java/asiantech/internship/summer/intro/TheJourneyBegins.java
@@ -3,6 +3,7 @@ package asiantech.internship.summer.intro;
 public class TheJourneyBegins {
     public static void main(String[] args) {
         add(1, 2);
+        centuryFromYear(2019);
     }
 
     public static int add(int param1, int param2) {
@@ -12,4 +13,17 @@ public class TheJourneyBegins {
         return param1 + param2;
     }
 
+    public static int centuryFromYear(int year) {
+        /**
+         * Given a year, return the century it is in.
+         The first century spans from the year 1 up to and including the year 100,
+         the second - from the year 101 up to and including the year 200, etc.
+         */
+        int century = year / 100;
+        int residual = year % 100;
+        if (residual != 0) {
+            century = century + 1;
+        }
+        return (century);
+    }
 }
