8cb2f4eeb85f61d4e7fd1d21383dbd80d2c59476
at-cuongle <cuong.le@asiantech.vn>
2019-12-27 11:23:08 +0700

    i forgot insert Threads, this commit to add Threads

--- app/src/at-cuongle/java/asiantech/internship/summer/intro/RainsofReason.java
+++ app/src/at-cuongle/java/asiantech/internship/summer/intro/RainsofReason.java
@@ -6,6 +6,9 @@ public class RainsofReason {
     }
 
     public static int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+        /**
+         * Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+         */
         for (int i = 0; i < inputArray.length; i++) {
             if (inputArray[i] == elemToReplace) {
                 inputArray[i] = substitutionElem;
@@ -15,6 +18,9 @@ public class RainsofReason {
     }
 
     public static boolean evenDigitsOnly(int n) {
+        /**
+         * Check if all digits of the given integer are even.
+         */
         int getEachNumber = 0;
         int coppyNumber = n;
         do {
@@ -29,6 +35,12 @@ public class RainsofReason {
     }
 
     public static boolean variableName(String name) {
+        /**
+         * Correct variable names consist only of English letters, digits
+         * and underscores and they can't start with a digit.
+         *
+         * Check if the given string is a correct variable name.
+         */
         if (name.length() < 0) {
             return false;
         }
@@ -37,6 +49,10 @@ public class RainsofReason {
     }
 
     public static String alphabeticShift(String inputString) {
+        /**
+         * Given a string, your task is to replace each of its characters by the next one
+         * in the English alphabet; i.e. replace a with b, replace b with c, etc (z would be replaced by a).
+         */
         char[] toCharArray = inputString.toCharArray();
         for (int i = 0; i < inputString.length(); i++) {
             int toAscii = toCharArray[i];
@@ -51,6 +67,9 @@ public class RainsofReason {
     }
 
     public static boolean chessBoardCellColor(String cell1, String cell2) {
+        /**
+         * Given two cells on the standard chess board, determine whether they have the same color or not.
+         */
         return (cell1.charAt(0) + cell1.charAt(1)) % 2 == 0 &&
                 (cell2.charAt(0) + cell2.charAt(1)) % 2 == 0 ||
                 (cell1.charAt(0) + cell1.charAt(1)) % 2 != 0 &&
