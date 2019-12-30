661e4065d56c44ae1b877f703e02cbe4b3a175cc
haxuannguyen1997201 <haxuannguyen1997201@gmail.com>
2019-12-30 09:01:04 +0700

    RainofReason all Exam

---
+++ app/src/at-nguyenha/java/asiantech/internship/summer/intro/RainsofReason.java
@@ -0,0 +1,96 @@
+package asiantech.internship.summer.intro;
+
+public class RainsofReason {
+
+
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * 25
+     * Given an array of integers, replace all the occurrences of elemToReplace with substitutionElem.
+     */
+    int[] arrayReplace(int[] inputArray, int elemToReplace, int substitutionElem) {
+        int size = inputArray.length;
+        int sub = substitutionElem;
+        for(int i = 0; i<size; i++){
+            if(inputArray[i] == elemToReplace){
+                inputArray[i] = sub;
+            }
+        }
+        return inputArray;
+    }
+
+
+    /**
+     * 26
+     * Check if all digits of the given integer are even.
+     */
+    boolean evenDigitsOnly(int n) {
+        int count = 0;
+        while(n>=1){
+            if(n % 2 != 0){
+                count++;
+            }
+            n /= 10;
+        }
+        if(count >= 1){
+            return false;
+        }
+        return true;
+    }
+
+
+    /**
+     * 27
+     * Correct variable names consist only of English letters,
+     * digits and underscores and they can't start with a digit.
+     *
+     * Check if the given string is a correct variable name.
+     */
+    boolean variableName(String name) {
+        if(name.matches("[a-zA-Z_][0-9a-zA-Z_]*$")){
+            return true;
+        }
+        return false;
+    }
+
+
+    /**
+     * 28
+     * Given a string, your task is to replace each of its
+     * characters by the next one in the English alphabet;
+     * i.e. replace a with b, replace b with c, etc
+     * (z would be replaced by a).
+     */
+    String alphabeticShift(String inputString) {
+        StringBuilder sb = new StringBuilder();
+        int size = inputString.length();
+        for(int i =0; i < size; i++){
+            char c = inputString.charAt(i);
+            if(c == 'z'){
+                sb.append('a');
+            } else{
+                sb.append((char) (inputString.charAt(i) + 1));
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Given two cells on the standard chess board,
+     * determine whether they have the same color or not.
+     */
+    boolean chessBoardCellColor(String cell1, String cell2) {
+        if((cell1.charAt(0) + cell1.charAt(1)) % 2 == 0 &&
+                (cell2.charAt(0) + cell2.charAt(1)) % 2 == 0 ||
+                        (cell1.charAt(0) + cell1.charAt(1)) % 2 != 0 &&
+                                (cell2.charAt(0) + cell2.charAt(1)) % 2 != 0){
+            return true;
+        }
+        return false;
+    }
+
+
+}
