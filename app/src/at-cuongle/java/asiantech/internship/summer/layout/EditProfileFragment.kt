d062c4f7848227480a41b4b46d533da6afdfc3c1
at-cuongle <cuong.le@asiantech.vn>
2020-01-14 14:08:52 +0700

    fix Edit profile activity

--- app/src/at-cuongle/java/asiantech/internship/summer/layout/EditProfileFragment.kt
+++ app/src/at-cuongle/java/asiantech/internship/summer/layout/EditProfileFragment.kt
@@ -2,16 +2,19 @@ package asiantech.internship.summer.layout
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
+
 /**
  * A simple [Fragment] subclass.
  */
 class EditProfileFragment : Fragment() {
-
+    companion object {
+        fun getInstance() = EditProfileFragment()
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
