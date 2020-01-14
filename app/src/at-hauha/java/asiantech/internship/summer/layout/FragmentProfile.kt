1f2e1a23addf38903fe1b1ef2a92f9429c5f5f85
at-hauha <hau.ha@asiantech.vn>
2020-01-14 11:23:05 +0700

    Add ProfileLayout

---
+++ app/src/at-hauha/java/asiantech/internship/summer/layout/FragmentProfile.kt
@@ -0,0 +1,17 @@
+package asiantech.internship.summer.layout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import asiantech.internship.summer.R
+
+class FragmentProfile:Fragment() {
+    companion object{
+        fun getInstance() = FragmentProfile()
+    }
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_profile,container,false)
+    }
+}
