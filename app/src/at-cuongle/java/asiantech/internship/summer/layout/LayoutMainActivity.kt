22399ea6cd538c74c74e6bf7e81f9107149b324b
at-cuongle <cuong.le@asiantech.vn>
2020-01-14 10:56:16 +0700

    fix login activity

---
+++ app/src/at-cuongle/java/asiantech/internship/summer/layout/LayoutMainActivity.kt
@@ -0,0 +1,19 @@
+package asiantech.internship.summer.layout
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import asiantech.internship.summer.R
+import asiantech.internship.summer.layout.LoginFragment.Companion.getInstance
+
+class LayoutMainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout_main)
+        supportFragmentManager.beginTransaction()
+                .add(R.id.flContainer, getInstance(), null)
+                .addToBackStack(null)
+                .commit()
+    }
+}
+
--- app/src/at-cuongle/java/asiantech/internship/summer/layout/LoginFragment.kt
+++ app/src/at-cuongle/java/asiantech/internship/summer/layout/LoginFragment.kt
@@ -2,10 +2,10 @@ package asiantech.internship.summer.layout
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
 
 //import asiantech.internship.summer.cuongle.debug.R
@@ -14,12 +14,13 @@ import asiantech.internship.summer.R
  * A simple [Fragment] subclass.
  */
 class LoginFragment : Fragment() {
-
+    companion object {
+        fun getInstance() = LoginFragment()
+    }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
-
 
 }
