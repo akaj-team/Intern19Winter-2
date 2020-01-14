a1d92142efed4c053e5d5b41f4e16a1855da8785
at-hauha <hau.ha@asiantech.vn>
2020-01-14 11:21:54 +0700

    Add RegistryLayout

---
+++ app/src/at-hauha/java/asiantech/internship/summer/layout/FragementRegistry.kt
@@ -0,0 +1,15 @@
+package asiantech.internship.summer.layout
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import asiantech.internship.summer.R
+
+class FragementRegistry:Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_registry,container,false)
+    }
+}
