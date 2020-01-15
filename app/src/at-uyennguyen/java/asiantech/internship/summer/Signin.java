f2b044fb29f28c4e02c4b212d5aa964e1f19424b
= <=>
2020-01-15 10:55:47 +0700

    Layout Login Signin Profile

---
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/Signin.java
@@ -0,0 +1,18 @@
+package asiantech.internship.summer;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class Signin extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.signin,container,false);
+    }
+}
