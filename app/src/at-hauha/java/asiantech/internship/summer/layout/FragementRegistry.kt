78057730d70c6fc41f971073749c80b7df3ec5dc
at-hauha <hau.ha@asiantech.vn>
2020-01-16 10:17:50 +0700

    Fix warnning

--- app/src/at-hauha/java/asiantech/internship/summer/layout/FragementRegistry.kt
+++ app/src/at-hauha/java/asiantech/internship/summer/layout/FragementRegistry.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
+import kotlinx.android.synthetic.`at-hauha`.fragment_registry.*
 
 class FragementRegistry:Fragment() {
     companion object{
@@ -14,5 +15,16 @@ class FragementRegistry:Fragment() {
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_registry,container,false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtLogin.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                    ?.replace(R.id.flContainer,FragmentLogin.getInstance(),null)
+                    ?.addToBackStack(null)
+                    ?.commit()
+        }
+
     }
 }
--- app/src/at-hauha/java/asiantech/internship/summer/layout/FragmentLogin.kt
+++ app/src/at-hauha/java/asiantech/internship/summer/layout/FragmentLogin.kt
@@ -6,6 +6,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
+import kotlinx.android.synthetic.`at-hauha`.fragment_login.*
+import kotlinx.android.synthetic.`at-hauha`.fragment_registry.*
+import kotlinx.android.synthetic.`at-hauha`.fragment_registry.txtCreateAccount
+import kotlinx.android.synthetic.`at-hauha`.fragment_login.txtLogin as txtLogin1
 
 class FragmentLogin : Fragment(){
     companion object{
@@ -13,5 +17,20 @@ class FragmentLogin : Fragment(){
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        txtCreateAccount.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                    ?.replace(R.id.flContainer,FragmentProfile.getInstance(),null)
+                    ?.addToBackStack(null)
+                    ?.commit()
+        }
+        txtLogin.setOnClickListener {
+            fragmentManager?.beginTransaction()
+                    ?.replace(R.id.flContainer,FragmentProfile.getInstance(),null)
+                    ?.addToBackStack(null)
+                    ?.commit()
+        }
     }
 }
