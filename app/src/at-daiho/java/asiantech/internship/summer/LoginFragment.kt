0a7c687871508819716bd69eb4a5953f5ef083a3
Dai Ho V <dai.ho@asiantech.vn>
2020-01-30 14:58:07 +0700

    Convert Login/SignUp to Fragment

---
+++ app/src/at-daiho/java/asiantech/internship/summer/LoginFragment.kt
@@ -0,0 +1,25 @@
+package asiantech.internship.summer
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.`at-daiho`.fragment_edit_profile.*
+
+
+class LoginFragment : Fragment() {
+
+    companion object {
+        fun getInstance() = LoginFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
---
+++ app/src/at-daiho/java/asiantech/internship/summer/SignUpFragment.kt
@@ -0,0 +1,21 @@
+package asiantech.internship.summer
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.`at-daiho`.fragment_edit_profile.*
+
+class SignUpFragment : Fragment() {
+
+    companion object {
+        fun getInstance() = SignUpFragment()
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_sign_up, container, false)
+    }
+}
