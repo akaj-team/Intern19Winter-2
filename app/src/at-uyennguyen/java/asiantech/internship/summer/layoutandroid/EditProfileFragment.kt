b7c5041a7c55ce6089682fd5549ec173914cc11e
= <=>
2020-02-03 15:49:55 +0700

    add test code

--- app/src/at-uyennguyen/java/asiantech/internship/summer/layoutandroid/EditProfileFragment.kt
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/layoutandroid/EditProfileFragment.kt
@@ -7,107 +7,18 @@ import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.os.Bundle
 import android.provider.MediaStore
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
 import com.google.android.material.snackbar.Snackbar
 import de.hdodenhof.circleimageview.CircleImageView
 import kotlinx.android.synthetic.`at-uyennguyen`.fragment_edit_profile.*
-import kotlinx.android.synthetic.`at-uyennguyen`.fragment_user_profile.*
 
 
 class EditProfileFragment : Fragment(){
-//    lateinit var changeAvatar: ChangeAvatar
-//    lateinit var bitMap: Bitmap
-//
-//    companion object {
-//        const val CAMERA_REQUEST_CODE = 101
-//        const val WRITE_CODE = 102
-//        val FULLNAME:String="fullname"
-//        val EMAIL:String="email"
-//        fun profileEdit(name:String,email:String): EditProfileFragment {
-//            val editProfileFragment=EditProfileFragment()
-//            val bundle=Bundle()
-//            bundle.putString(FULLNAME,name)
-//            bundle.putString(EMAIL,email)
-//            editProfileFragment.arguments=bundle
-//            return editProfileFragment
-//        }
-//    }
-//
-//    //private lateinit var output: File
-//
-//    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-//        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
-//    }
-//
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        editFullName.setText(arguments?.getString(FULLNAME))
-//        editViewEmail2.setText(arguments?.getString(EMAIL))
-//        youravatar.setImageBitmap(bitMap)
-//        super.onViewCreated(view, savedInstanceState)
-//        youravatar.setOnClickListener(object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), CAMERA_REQUEST_CODE) // tại sao chỉ có code camera mà ko có code write
-//                //requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_CODE)
-//                //hỏi quyền
-//            }
-//        })
-//        textBack.setOnClickListener(object : View.OnClickListener{
-//            override fun onClick(v: View?) {
-//                this@EditProfileFragment.fragmentManager?.popBackStack()
-//            }
-//        })
-//        btnProfile.setOnClickListener(object : View.OnClickListener {
-//            override fun onClick(v: View?) {
-//                Log.d("change", changeAvatar.toString())
-//                changeAvatar.profile(editFullName.text.toString(),editBio.text.toString())
-//                if (bitMap != null) {
-//                    changeAvatar.imageUser(bitMap)
-//                }
-//                this@EditProfileFragment.fragmentManager?.popBackStack()
-//            }
-//        })
-//    }
-//
-//    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
-//        if (requestCode == CAMERA_REQUEST_CODE && grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-//            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
-//            startActivityForResult(intent, CAMERA_REQUEST_CODE)
-//            // mở camera
-//        } else {
-//            if (!this@EditProfileFragment.shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
-//                view?.apply {
-//                    val snackbar =
-//                            Snackbar.make(this, R.string.message_no_storage_permission_snackbar, Snackbar.LENGTH_LONG)
-//                    snackbar.setAction(R.string.settings, object : View.OnClickListener {
-//                        override fun onClick(v: View?) {
-//                            this@EditProfileFragment.startActivityForResult(Intent(android.provider.Settings.ACTION_SETTINGS), 0)
-//                        }
-//                    })
-//                    snackbar.show()
-//                }
-//            }
-//        }
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//    }
-//
-//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-//       //changeAvatar = UserProfileFragment()
-//            if (requestCode == CAMERA_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
-//                bitMap = data.extras?.get("data") as Bitmap // data luon la mac dinh
-//                youravatar.setImageBitmap(bitMap)
-////                val fileOutputStream = FileOutputStream(Environment.getExternalStorageDirectory().absolutePath+"/savepicture.png")
-////                bitMap.compress(Bitmap.CompressFormat.PNG,100,fileOutputStream)
-//            }
-//            super.onActivityResult(requestCode, resultCode, data)
-//
-//    }
-lateinit var changeAvatar: ChangeAvatar
+//lateinit var changeAvatar: ChangeAvatar
     lateinit var bitMap: Bitmap
 
     companion object {
@@ -115,20 +26,19 @@ lateinit var changeAvatar: ChangeAvatar
         const val WRITE_CODE = 102
         val FULLNAME:String="fullname"
         val EMAIL:String="email"
-        val BIO:String="bio"
-        fun profileEdit(name:String,email:String,bio:String): EditProfileFragment {
+        val BIO:String=""
+        val IMG:Bitmap?=null
+        fun profileEdit(name:String, email:String, bio:String, img: CircleImageView): EditProfileFragment {
             val editProfileFragment=EditProfileFragment()
             val bundle=Bundle()
             bundle.putString(FULLNAME,name)
             bundle.putString(BIO,bio)
             bundle.putString(EMAIL,email)
+            bundle.putString(IMG.toString(),img.toString())
             editProfileFragment.arguments=bundle
             return editProfileFragment
         }
     }
-
-    //private lateinit var output: File
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
@@ -137,13 +47,11 @@ lateinit var changeAvatar: ChangeAvatar
         editFullName.setText(arguments?.getString(FULLNAME))
         editViewEmail2.setText(arguments?.getString(EMAIL))
         editBio.setText(arguments?.getString(BIO))
-        youravatar.setImageBitmap(bitMap)
+        //youravatar.setImageBitmap(bitMap)
         super.onViewCreated(view, savedInstanceState)
         youravatar.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), CAMERA_REQUEST_CODE) // tại sao chỉ có code camera mà ko có code write
-                //requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), WRITE_CODE)
-                //hỏi quyền
+                requestPermissions(arrayOf(Manifest.permission.CAMERA, Manifest.permission.WRITE_EXTERNAL_STORAGE), CAMERA_REQUEST_CODE)
             }
         })
         textBack.setOnClickListener(object : View.OnClickListener{
@@ -153,11 +61,17 @@ lateinit var changeAvatar: ChangeAvatar
         })
         btnProfile.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                Log.d("change", changeAvatar.toString())
-                changeAvatar.profile(editFullName.text.toString(),editBio.text.toString())
-                if (bitMap != null) {
-                    changeAvatar.imageUser(bitMap)
-                }
+//                changeAvatar.profile(editFullName.text.toString(),editBio.text.toString())
+//                if (bitMap != null) {
+//                    changeAvatar.imageUser(bitMap)
+//                }
+                val userProfileFragment: UserProfileFragment = UserProfileFragment()
+                //editProfileFragment.changeAvatar = this@UserProfileFragment
+                //editProfileFragment.bitMap = (avatar.getDrawable() as BitmapDrawable).getBitmap()
+                activity?.supportFragmentManager?.beginTransaction()
+                        ?.add(R.id.frameLayout, userProfileFragment)
+                        ?.addToBackStack(null)
+                        ?.commit()
                 this@EditProfileFragment.fragmentManager?.popBackStack()
             }
         })
--- app/src/at-uyennguyen/java/asiantech/internship/summer/layoutandroid/UserProfileFragment.kt
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/layoutandroid/UserProfileFragment.kt
@@ -1,7 +1,6 @@
 package asiantech.internship.summer.layoutandroid
 
 import android.graphics.Bitmap
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -10,18 +9,20 @@ import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
 import kotlinx.android.synthetic.`at-uyennguyen`.fragment_user_profile.*
 
-class UserProfileFragment : Fragment(), ChangeAvatar {
-
-    override fun profile(fullname: String, bio: String) {
-        name.setText(fullname)
-        address.setText(bio)
-    }
-    override fun imageUser(img: Bitmap?) {
-        avatar.setImageBitmap(img)
-    }
+class UserProfileFragment : Fragment(){
+//, ChangeAvatar
+//    override fun profile(fullname: String, bio: String) {
+//        name.setText(fullname)
+//        address.setText(bio)
+//    }
+//    override fun imageUser(img: Bitmap?) {
+//        avatar.setImageBitmap(img)
+//    }
     companion object{
         val NAME:String="name"
         val EMAIL:String="email"
+        val BIO:String="bio"
+        val IMG:Bitmap?=null
         fun newProfile(name:String,email:String): UserProfileFragment {
             val userProfileFragment=UserProfileFragment()
             val bundle=Bundle()
@@ -30,6 +31,15 @@ class UserProfileFragment : Fragment(), ChangeAvatar {
             userProfileFragment.arguments=bundle
             return userProfileFragment
         }
+//        fun loadProfile(name: String,bio: String,img: Bitmap?): UserProfileFragment {
+//            val userProfileFragment=UserProfileFragment()
+//            val bundle=Bundle()
+//            bundle.putString(NAME2,name)
+//            bundle.putString(BIO,bio)
+//            bundle.putString(IMG.toString(),img.toString())
+//            userProfileFragment.arguments=bundle
+//            return userProfileFragment
+//        }
     }
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_user_profile, container, false)
@@ -40,11 +50,16 @@ class UserProfileFragment : Fragment(), ChangeAvatar {
         var emailEdit:String
         emailEdit= arguments?.getString(EMAIL).toString()
         name.setText(arguments?.getString(NAME))
+//
+//        avatar.setImageBitmap(IMG)
+//        name.setText(NAME2)
+//        address.setText(BIO)
+
         penciledit.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
-                val editProfileFragment: EditProfileFragment = EditProfileFragment.profileEdit(name.text.toString(),emailEdit,address.text.toString())
-                editProfileFragment.changeAvatar = this@UserProfileFragment
-                editProfileFragment.bitMap = (avatar.getDrawable() as BitmapDrawable).getBitmap()
+                val editProfileFragment: EditProfileFragment = EditProfileFragment.profileEdit(name.text.toString(),emailEdit,address.text.toString(),avatar)
+                //editProfileFragment.changeAvatar = this@UserProfileFragment
+                //editProfileFragment.bitMap = (avatar.getDrawable() as BitmapDrawable).getBitmap()
                 activity?.supportFragmentManager?.beginTransaction()
                         ?.add(R.id.frameLayout, editProfileFragment)
                         ?.addToBackStack(null)
