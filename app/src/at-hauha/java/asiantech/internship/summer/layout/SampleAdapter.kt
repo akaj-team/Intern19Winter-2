96be7c151c31e2832601ffe6f16a97a6c93a9023
Hau Ha C <59002115+at-hauha@users.noreply.github.com>
2020-02-11 17:30:21 +0700

    Update SampleAdapter.kt

--- app/src/at-hauha/java/asiantech/internship/summer/layout/SampleAdapter.kt
+++ app/src/at-hauha/java/asiantech/internship/summer/layout/SampleAdapter.kt
@@ -1,14 +1,13 @@
 package asiantech.internship.summer.layout
 
-
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentStatePagerAdapter
 
 class SampleAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
+    
     private val mFragmentList: ArrayList<Fragment> = ArrayList()
     private val mFragmentTitleList: ArrayList<String> = ArrayList()
-
 
     override fun getItem(position: Int): Fragment {
         return mFragmentList[position]
