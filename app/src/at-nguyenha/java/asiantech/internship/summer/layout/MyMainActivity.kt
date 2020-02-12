243ee899c3e28e2c87be6040ea21a7cbdfa7eb76
at-nguyenha <nguyen.ha@asiantech.vn>
2020-02-12 08:43:44 +0700

    Change file MyMainActivity , add file activity_layout, fix id of guideline

--- app/src/at-nguyenha/java/asiantech/internship/summer/layout/MyMainActivity.kt
+++ app/src/at-nguyenha/java/asiantech/internship/summer/layout/MyMainActivity.kt
@@ -1,18 +1,17 @@
 package asiantech.internship.summer.layout
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import asiantech.internship.summer.R
 
-class MyMainActiviry : AppCompatActivity() {
+class MyMainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_layout)
         supportFragmentManager.beginTransaction()
                 .add(R.id.frLayout, RegisterFragment(), null)
                 .addToBackStack(null)
                 .commit()
-
     }
 }
