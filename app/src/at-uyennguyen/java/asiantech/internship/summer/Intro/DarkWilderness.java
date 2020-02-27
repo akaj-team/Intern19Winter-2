0e730a8567dcf4b839c24826be741a6bebecafd0
at-uyennguyen <=>
2020-02-27 10:06:58 +0700

    Fix file change

--- app/src/at-uyennguyen/java/asiantech/internship/summer/Intro/DarkWilderness.java
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/Intro/DarkWilderness.java
@@ -21,8 +21,7 @@ public class DarkWilderness {
      * You found two items in a treasure chest!
      * The first item weighs weight1 and is worth value1, and the second item weighs weight2 and is worth value2.
      * What is the total maximum value of the items you can take with you,
-     * assuming that your max weight capacity is maxW and you can't come back
-     * for the items later?
+     * assuming that your max weight capacity is maxW and you can't come back for the items later?
      */
     static int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
         if (weight1 + weight2 <= maxW) {
