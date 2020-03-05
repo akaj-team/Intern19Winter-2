cd3830f2b74ac39ef4d9123f95b8b6cb65a3f59e
at-uyennguyen <=>
2020-03-05 16:30:23 +0700

    Fix CI

--- app/src/at-uyennguyen/java/asiantech/internship/summer/apptodolist/AddTodoFragment.kt
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/apptodolist/AddTodoFragment.kt
@@ -9,7 +9,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import asiantech.internship.summer.R
-import kotlinx.android.synthetic.main.fragment_add_todo.*
+import kotlinx.android.synthetic.`at-uyennguyen`.fragment_add_todo.*
 
 class AddTodoFragment : Fragment() {
 
