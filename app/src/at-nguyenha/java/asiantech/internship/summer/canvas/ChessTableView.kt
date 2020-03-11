4e2f3752d225653779f32276887d2c881115fa36
at-nguyenha <nguyen.ha@asiantech.vn>
2020-03-11 17:50:14 +0700

    edit size from px to width of devide, conplete to drawing chess table

--- app/src/at-nguyenha/java/asiantech/internship/summer/canvas/ChessTableView.kt
+++ app/src/at-nguyenha/java/asiantech/internship/summer/canvas/ChessTableView.kt
@@ -13,57 +13,89 @@ class ChessTableView(context: Context?, attrs: AttributeSet?) : View(context, at
     private var borderWidthIn = 4.0f
     private var borderWidthOut = 10.0f
     private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
-
-//    private fun init(@Nullable set: AttributeSet) {
-//    }
+    private var size: Float = 0f
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+        size = (width / 10).toFloat()
         drawBorderOut(canvas)
         drawBorderIn(canvas)
+        drawVerticalLine(canvas)
+        drawHorizontalLine(canvas)
+        drawDiagonalLine(size * 5, size * 2, canvas)
+        drawDiagonalLine(size * 5, size * 9, canvas)
+        drawSmallLine(size * 2, size * 3, canvas)
+        drawSmallLine(size * 8, size * 3, canvas)
+        drawSmallLine(size * 2, size * 8, canvas)
+        drawSmallLine(size * 8, size * 8, canvas)
+        drawSmallLine(size * 3, size * 4, canvas)
+        drawSmallLine(size * 5, size * 4, canvas)
+        drawSmallLine(size * 7, size * 4, canvas)
+        drawSmallLine(size * 3, size * 7, canvas)
+        drawSmallLine(size * 5, size * 7, canvas)
+        drawSmallLine(size * 7, size * 7, canvas)
+        drawLeftPawn(size, size * 4, canvas)
+        drawLeftPawn(size, size * 7, canvas)
+        drawRightPawn(size * 9, size * 4, canvas)
+        drawRightPawn(size * 9, size * 7, canvas)
 
-        //draw horizontal line
-        canvas.drawLine(90f, 202.5f, 990f, 202.5f, paint)
-        canvas.drawLine(90f, 315f, 990f, 315f, paint)
-        canvas.drawLine(90f, 427.5f, 990f, 427.5f, paint)
-        canvas.drawLine(90f, 540f, 990f, 540f, paint)
-        canvas.drawLine(90f, 652.5f, 990f, 652.5f, paint)
-        canvas.drawLine(90f, 765f, 990f, 765f, paint)
-        canvas.drawLine(90f, 877.5f, 990f, 877.5f, paint)
-        canvas.drawLine(90f, 990f, 990f, 990f, paint)
-
-        //draw vertical line
-        canvas.drawLine(202.5f, 90f, 202.5f, 540f, paint)
-        canvas.drawLine(202.5f, 652.5f, 202.5f, 1102.5f, paint)
-        canvas.drawLine(315f, 90f, 315f, 540f, paint)
-        canvas.drawLine(315f, 652.5f, 315f, 1102.5f, paint)
-        canvas.drawLine(427.5f, 90f, 427.5f, 540f, paint)
-        canvas.drawLine(427.5f, 652.5f, 427.5f, 1102.5f, paint)
-        canvas.drawLine(540f, 90f, 540f, 540f, paint)
-        canvas.drawLine(540f, 652.5f, 540f, 1102.5f, paint)
-        canvas.drawLine(652.5f, 90f, 652.5f, 540f, paint)
-        canvas.drawLine(652.5f, 652.5f, 652.5f, 1102.5f, paint)
-        canvas.drawLine(765f, 90f, 765f, 540f, paint)
-        canvas.drawLine(765f, 652.5f, 765f, 1102.5f, paint)
-        canvas.drawLine(877.5f, 90f, 877.5f, 540f, paint)
-        canvas.drawLine(877.5f, 652.5f, 877.5f, 1102.5f, paint)
-
-        //draw diagonal line
-        canvas.drawLine(427.5f, 90f, 652.5f, 315f, paint)
-        canvas.drawLine(427.5f, 315f, 652.5f, 90f, paint)
-        canvas.drawLine(427.5f, 1102.5f, 652.5f, 877.5f, paint)
-        canvas.drawLine(427.5f, 877.5f, 652.5f, 1102.5f, paint)
     }
+
+    //    private fun init(@Nullable set: AttributeSet) {
+//    }
 
     private fun drawBorderOut(canvas: Canvas) {
         paint.color = lineColor
         paint.style = Paint.Style.STROKE
         paint.strokeWidth = borderWidthOut
-        canvas.drawRect(70F, 70F, 1010F, 1122.5F, paint)
+        canvas.drawRect(size - 20f, size - 20f, size * 9 + 20f, size * 10 + 20f, paint)
     }
 
     private fun drawBorderIn(canvas: Canvas) {
         paint.strokeWidth = borderWidthIn
-        canvas.drawRect(90F, 90F, 990F, 1102.5F, paint)
+        canvas.drawRect(size, size, size * 9, size * 10, paint)
+    }
+
+    private fun drawSmallLine(dX: Float, dY: Float, canvas: Canvas) {
+        canvas.drawLine(dX - 10, dY - 10, dX - 30, dY - 10, paint)
+        canvas.drawLine(dX - 10, dY + 10, dX - 30, dY + 10, paint)
+        canvas.drawLine(dX + 10, dY - 10, dX + 30, dY - 10, paint)
+        canvas.drawLine(dX + 10, dY + 10, dX + 30, dY + 10, paint)
+        canvas.drawLine(dX - 10, dY - 10, dX - 10, dY - 30, paint)
+        canvas.drawLine(dX - 10, dY + 10, dX - 10, dY + 30, paint)
+        canvas.drawLine(dX + 10, dY - 10, dX + 10, dY - 30, paint)
+        canvas.drawLine(dX + 10, dY + 10, dX + 10, dY + 30, paint)
+    }
+
+    private fun drawLeftPawn(dX: Float, dY: Float, canvas: Canvas) {
+        canvas.drawLine(dX + 10, dY - 10, dX + 30, dY - 10, paint)
+        canvas.drawLine(dX + 10, dY + 10, dX + 30, dY + 10, paint)
+        canvas.drawLine(dX + 10, dY - 30, dX + 10, dY - 10, paint)
+        canvas.drawLine(dX + 10, dY + 30, dX + 10, dY + 10, paint)
+    }
+
+    private fun drawRightPawn(dX: Float, dY: Float, canvas: Canvas) {
+        canvas.drawLine(dX - 10, dY - 10, dX - 30, dY - 10, paint)
+        canvas.drawLine(dX - 10, dY + 10, dX - 30, dY + 10, paint)
+        canvas.drawLine(dX - 10, dY - 30, dX - 10, dY - 10, paint)
+        canvas.drawLine(dX - 10, dY + 30, dX - 10, dY + 10, paint)
+    }
+
+    private fun drawHorizontalLine(canvas: Canvas) {
+        for (i in 2..9) {
+            canvas.drawLine(size, size * i, size * 9, size * i, paint)
+        }
+    }
+
+    private fun drawVerticalLine(canvas: Canvas) {
+        for (i in 2..8) {
+            canvas.drawLine(size * i, size * 1, size * i, size * 5, paint)
+            canvas.drawLine(size * i, size * 6, size * i, size * 10, paint)
+        }
+    }
+
+    private fun drawDiagonalLine(dX: Float, dY: Float, canvas: Canvas) {
+        canvas.drawLine(dX - size, dY - size, dX + size, dY + size, paint)
+        canvas.drawLine(dX - size, dY + size, dX + size, dY - size, paint)
     }
 }
