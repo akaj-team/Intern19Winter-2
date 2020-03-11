28458a68f0ebf395496df8a751de219cd0ad7dd4
at-uyennguyen <=>
2020-03-11 17:59:25 +0700

    Compact code

--- app/src/at-uyennguyen/java/asiantech/internship/summer/canvas/CanvasActivity.kt
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/canvas/CanvasActivity.kt
@@ -4,7 +4,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import asiantech.internship.summer.R
 
-class CanvasActivity : AppCompatActivity(){
+class CanvasActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas)
--- app/src/at-uyennguyen/java/asiantech/internship/summer/canvas/CheckView.kt
+++ app/src/at-uyennguyen/java/asiantech/internship/summer/canvas/CheckView.kt
@@ -18,30 +18,17 @@ class CheckView(context: Context, attributes: AttributeSet) : View(context, attr
         canvas?.drawLine(10f, width.toFloat() + (width.toFloat() / 8), width.toFloat(), width.toFloat() + (width.toFloat() / 8), paint)
         canvas?.drawLine(width.toFloat() - 2, 10f, width.toFloat() - 2, width.toFloat() + (width.toFloat() / 8), paint)
         //9 đường ngang
-        canvas?.drawLine(10f, width.toFloat() / 8, width.toFloat(), width.toFloat() / 8, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 2, width.toFloat(), (width.toFloat() / 8) * 2, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 3, width.toFloat(), (width.toFloat() / 8) * 3, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 4, width.toFloat(), (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 5, width.toFloat(), (width.toFloat() / 8) * 5, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 6, width.toFloat(), (width.toFloat() / 8) * 6, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 7, width.toFloat(), (width.toFloat() / 8) * 7, paint)
-        canvas?.drawLine(10f, (width.toFloat() / 8) * 8, width.toFloat(), (width.toFloat() / 8) * 8, paint)
+        for (i in 1..8) {
+            canvas?.drawLine(10f, width.toFloat() / 8 * i, width.toFloat(), width.toFloat() / 8 * i, paint)
+        }
         //8 đường dọc trên
-        canvas?.drawLine(width.toFloat() / 8, 10f, width.toFloat() / 8, (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 2, 10f, (width.toFloat() / 8) * 2, (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 3, 10f, (width.toFloat() / 8) * 3, (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 4, 10f, (width.toFloat() / 8) * 4, (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 5, 10f, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 6, 10f, (width.toFloat() / 8) * 6, (width.toFloat() / 8) * 4, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 7, 10f, (width.toFloat() / 8) * 7, (width.toFloat() / 8) * 4, paint)
+        for (i in 1..7) {
+            canvas?.drawLine(width.toFloat() / 8 * i, 10f, width.toFloat() / 8 * i, (width.toFloat() / 8) * 4, paint)
+        }
         //8 đường dọc dưới
-        canvas?.drawLine(width.toFloat() / 8, (width.toFloat() / 8) * 5, width.toFloat() / 8, (width.toFloat() / 8) * 9, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 2, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 2, (width.toFloat() / 8) * 9, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 3, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 3, (width.toFloat() / 8) * 9, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 4, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 4, (width.toFloat() / 8) * 9, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 5, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 9, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 6, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 6, (width.toFloat() / 8) * 9, paint)
-        canvas?.drawLine((width.toFloat() / 8) * 7, (width.toFloat() / 8) * 5, (width.toFloat() / 8) * 7, (width.toFloat() / 8) * 9, paint)
+        for (i in 1..7) {
+            canvas?.drawLine(width.toFloat() / 8 * i, (width.toFloat() / 8) * 5, width.toFloat() / 8 * i, (width.toFloat() / 8) * 9, paint)
+        }
         //2 đường chéo trên
         canvas?.drawLine(width.toFloat() / 8 * 3, 10f, width.toFloat() / 8 * 5, width.toFloat() / 8 * 2, paint)
         canvas?.drawLine(width.toFloat() / 8 * 5, 10f, width.toFloat() / 8 * 3, width.toFloat() / 8 * 2, paint)
@@ -60,25 +47,27 @@ class CheckView(context: Context, attributes: AttributeSet) : View(context, attr
             canvas?.drawLine(width.toFloat() / 8 * x + 10f, width.toFloat() / 8 * y - 10f, width.toFloat() / 8 * x + 40f, width.toFloat() / 8 * y - 10f, paint)
         }
 
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 3 + 10f, 10 + 10f, width.toFloat() / 8 * 3 + 40f, paint)
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 3 - 10f, 10 + 10f, width.toFloat() / 8 * 3 - 40f, paint)
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 3 + 10f, 10 + 40f, width.toFloat() / 8 * 3 + 10f, paint)
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 3 - 10f, 10 + 40f, width.toFloat() / 8 * 3 - 10f, paint)
+        fun twoCornersLeft(y: Int) {
+            canvas?.drawLine(10 + 10f, width.toFloat() / 8 * y + 10f, 10 + 10f, width.toFloat() / 8 * y + 40f, paint)
+            canvas?.drawLine(10 + 10f, width.toFloat() / 8 * y - 10f, 10 + 10f, width.toFloat() / 8 * y - 40f, paint)
+            canvas?.drawLine(10 + 10f, width.toFloat() / 8 * y + 10f, 10 + 40f, width.toFloat() / 8 * y + 10f, paint)
+            canvas?.drawLine(10 + 10f, width.toFloat() / 8 * y - 10f, 10 + 40f, width.toFloat() / 8 * y - 10f, paint)
+        }
 
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 7 + 10f, 10 + 10f, width.toFloat() / 8 * 7 + 40f, paint)
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 7 - 10f, 10 + 10f, width.toFloat() / 8 * 7 - 40f, paint)
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 7 + 10f, 10 + 40f, width.toFloat() / 8 * 7 + 10f, paint)
-        canvas?.drawLine(10 + 10f, width.toFloat() / 8 * 7 - 10f, 10 + 40f, width.toFloat() / 8 * 7 - 10f, paint)
+        fun twoCornersRight(y: Int) {
+            canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * y - 10f, width.toFloat() - 10f, width.toFloat() / 8 * y - 40f, paint)
+            canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * y + 10f, width.toFloat() - 10f, width.toFloat() / 8 * y + 40f, paint)
+            canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * y - 10f, width.toFloat() - 40f, width.toFloat() / 8 * y - 10f, paint)
+            canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * y + 10f, width.toFloat() - 40f, width.toFloat() / 8 * y + 10f, paint)
 
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 6 - 10f, width.toFloat() - 10f, width.toFloat() / 8 * 6 - 40f, paint)
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 6 + 10f, width.toFloat() - 10f, width.toFloat() / 8 * 6 + 40f, paint)
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 6 - 10f, width.toFloat() - 40f, width.toFloat() / 8 * 6 - 10f, paint)
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 6 + 10f, width.toFloat() - 40f, width.toFloat() / 8 * 6 + 10f, paint)
+        }
 
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 3 - 10f, width.toFloat() - 10f, width.toFloat() / 8 * 3 - 40f, paint)
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 3 + 10f, width.toFloat() - 10f, width.toFloat() / 8 * 3 + 40f, paint)
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 3 - 10f, width.toFloat() - 40f, width.toFloat() / 8 * 3 - 10f, paint)
-        canvas?.drawLine(width.toFloat() - 10f, width.toFloat() / 8 * 3 + 10f, width.toFloat() - 40f, width.toFloat() / 8 * 3 + 10f, paint)
+
+        twoCornersLeft(3)
+        twoCornersLeft(6)
+
+        twoCornersRight(6)
+        twoCornersRight(3)
 
         fourCorners(1, 2)
         fourCorners(7, 2)
