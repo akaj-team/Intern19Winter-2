322f7545a2c5cc25bb164d3cb4c8edb267a907f7
at-cuongle <cuong.le@asiantech.vn>
2020-03-11 16:56:06 +0700

    draw chess board

--- app/src/at-cuongle/java/asiantech/internship/summer/canvas/CanvasMainActivity.kt
+++ app/src/at-cuongle/java/asiantech/internship/summer/canvas/CanvasMainActivity.kt
@@ -9,5 +9,9 @@ class CanvasMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_canvas_main)
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.clContainer, ChessBoardFragment(), null)
+                .addToBackStack(null)
+                .commit()
     }
 }
---
+++ app/src/at-cuongle/java/asiantech/internship/summer/canvas/ChessBoardFragment.kt
@@ -0,0 +1,19 @@
+package asiantech.internship.summer.canvas
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import asiantech.internship.summer.R
+
+class ChessBoardFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_chess_board, container, false)
+    }
+
+
+}
