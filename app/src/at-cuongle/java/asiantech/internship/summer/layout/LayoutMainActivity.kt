3606519d9736c31a0c86f7a17b02a50aa87053b7
at-cuongle <cuong.le@asiantech.vn>
2020-03-16 08:55:40 +0700

    fix file change

--- app/src/at-cuongle/java/asiantech/internship/summer/layout/LayoutMainActivity.kt
+++ app/src/at-cuongle/java/asiantech/internship/summer/layout/LayoutMainActivity.kt
@@ -10,7 +10,7 @@ class LayoutMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_layout_main)
-        replaceFragment(EditProfileFragment.newInstance("Nick Evans", "asiantech@gmail.com", ""))
+        replaceFragment(UserProfileFragment.newInstance("Nick Evans", "asiantech@gmail.com", ""))
 
     }
 
